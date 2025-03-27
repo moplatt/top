ab5192c74a6af1281eb191758f0813f6ade48d2f
moplatt <moritz.plattner03@gmail.com>
2025-03-27 15:49:26 +0100

    kommentare & lat,lon,zoom variablen

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-var map = L.map('map').setView([29.71, -8.96], 11);
+/*
+Script für die Lieblingorte
+*/
+
+let lat = 29.71;
+let lon = -8.96;
+let zoom = 11
+
+// Karte initialisieren
+var map = L.map('map').setView([lat,lon ], zoom);
+//Hintergrundkarte definieren
+
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
-        var marker = L.marker([29.71, -8.96]).addTo(map);
+        // Marker zeichnen
+        var marker = L.marker([lat, lon]).addTo(map);
+        // Popup definieren und öffnen
         marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
