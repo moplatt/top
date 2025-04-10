8dd4f1e7345683b18587d5e5c515e21951df1269
moplatt <moritz.plattner03@gmail.com>
2025-04-10 11:28:33 +0200

    Marker in feature group geschrieben und als Layercontrol hinzugefügt; basemaps erstellt für Layercontrol

--- main.js
+++ main.js
@@ -203,10 +203,19 @@ const STOPS = [
 
 // Karte initialisieren
 let map = L.map('map');
-//Hintergrundkarte definieren
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+
+// Overlays definieren
+let overlays = {
+    marker: L.featureGroup().addTo(map), // .addto(map) um layer default zu checken
+};
+
+// Layercontrol
+L.control.layers({
+    "OpenStreetMap Mapnik": L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(map),
+    "OpenTopoMap": L.tileLayer.provider('OpenTopoMap'),
+    "Esri WorldImagery": L.tileLayer.provider('Esri.WorldImagery'),
+}, {
+    "Lieblingsorte": overlays.marker, // .addto(map) um layer default zu checken
 }).addTo(map);
 
 // Maßstab
@@ -218,7 +227,7 @@ L.control.scale({
 // loop über Etappen 
 for (let i = 0; i < STOPS.length; i++) {
     // console.log(STOPS[i]);
-    let marker = L.marker([STOPS[i].lat, STOPS[i].lng]).addTo(map);
+    let marker = L.marker([STOPS[i].lat, STOPS[i].lng]).addTo(overlays.marker);
 
     // Popup definieren
     marker.bindPopup(`
